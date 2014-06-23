7c8affa4c143ed2b2f6327f58d3ce4544b20b125
Higgins Dragon <higgins@higginsdragon.com>
2014-06-22 17:05:05 -0700

    requirejs optimization settings

---
+++ app/app.build.js
@@ -0,0 +1,17 @@
+({
+  appDir: '.',
+  dir: '../app-build',
+  baseUrl: 'js',
+  paths: {
+    jquery: 'empty:',
+    backbone: 'empty:',
+    underscore: 'empty:',
+    marked: 'empty:',
+    medium: 'empty:'
+  },
+  modules: [
+    {
+      name: 'main'
+    }
+  ]
+})
